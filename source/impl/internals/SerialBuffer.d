99a02b68a388f0e915eacfc687f7e7d3ff6daef8
CircuitCoder4696 <50537531+CircuitCoder4696@users.noreply.github.com>
2023-03-10 18:46:39 -0600

    .

--- source/impl/internals/SerialBuffer.d
+++ source/impl/internals/SerialBuffer.d
@@ -1,8 +1,35 @@
-module impl.internals.SerialBuffer;
+module impl.internal.SerialBuffer;
 
-import std.stdio;
-
-void main()
-{
-	writeln("Edit source/app.d to start your project.");
-}
+public class SerialBuffer {
+    private size_t rPtr,wPtr;
+    private void[] data;
+    public this(size_t size) {
+        this.data= new void[size];
+    };
+    public void wValue(T)(T value) {
+        uint size= sizeof(value);
+        void[] d0= (cast(void*) (&value))[0 .. size];
+        this.data[this.wPtr .. (this.wPtr + size)]= d0;
+        this.wPtr += size;
+    };
+    public void wArray(T)(T[] data) {
+        uint size= sizeof(data);
+        void[] d0= (cast(void*) (data.ptr))[0 .. size];
+        this.data[this.wPtr .. (this.wPtr + size)]= d0;
+        this.wPtr += size;
+    };
+    public T rValue(ValueT)(ref T value) {
+        uint len= value.length;
+        uint size= sizeof(value);
+        void* d0= this.data.ptr + this.rPtr;
+        T* d1= d0;
+        value= *d1;
+    };
+    public void wArray(T)(ref T data) {
+        uint len= value.length;
+        uint size= sizeof(data);
+        void* d0= this.data.ptr + this.rPtr;
+        T* d1= d0;
+        data= d1[0 .. len];
+    };
+};
