571f244294b2f48cac2c75da2f9d7c4578bcadf6
CircuitCoder4696 <50537531+CircuitCoder4696@users.noreply.github.com>
2023-03-10 19:04:53 -0600

    .

--- source/impl/internal/SerialBuffer.d
+++ source/impl/internal/SerialBuffer.d
@@ -7,27 +7,27 @@ public class SerialBuffer {
         this.data= new void[size];
     };
     public void wValue(T)(T value) {
-        uint size= sizeof(value);
+        uint size= data.sizeof;
         void[] d0= (cast(void*) (&value))[0 .. size];
         this.data[this.wPtr .. (this.wPtr + size)]= d0;
         this.wPtr += size;
     };
     public void wArray(T)(T[] data) {
-        uint size= sizeof(data);
+        uint size= data.sizeof;
         void[] d0= (cast(void*) (data.ptr))[0 .. size];
         this.data[this.wPtr .. (this.wPtr + size)]= d0;
         this.wPtr += size;
     };
     public T rValue(ValueT)(ref T value) {
         uint len= value.length;
-        uint size= sizeof(value);
+        uint size= data.sizeof;
         void* d0= this.data.ptr + this.rPtr;
         T* d1= d0;
         value= *d1;
     };
     public void wArray(T)(ref T data) {
         uint len= value.length;
-        uint size= sizeof(data);
+        uint size= data.sizeof;
         void* d0= this.data.ptr + this.rPtr;
         T* d1= d0;
         data= d1[0 .. len];
